0b7774a70e4c48cf78987ada1bf247cde09900e7
Niwarthana Nihathamani <niwarthana.2016285@iit.ac.lk>
2021-06-17 00:13:58 +0530

    first page done

---
+++ lib/models/instructor.dart
@@ -0,0 +1,55 @@
+class Instructor {
+  String instructorName;
+  String rating;
+  String instructorPic;
+
+  Instructor({required this.instructorName, required this.rating, required this.instructorPic});
+}
+
+var allInstructors = [
+  Instructor(
+      instructorName: 'Cora Wong',
+      rating: '9.95',
+      instructorPic: 'assets/woman1.jpeg'
+  ),
+  Instructor(
+      instructorName: 'Charlie Roy',
+      rating: '9.91',
+      instructorPic: 'assets/man1.jpeg'
+  ),
+  Instructor(
+      instructorName: 'Ruth Norton',
+      rating: '9.87',
+      instructorPic: 'assets/woman2.jpeg'
+  ),
+  Instructor(
+      instructorName: 'Leah Price',
+      rating: '9.85',
+      instructorPic: 'assets/man2.jpeg'
+  ),
+  Instructor(
+      instructorName: 'Hettie Bell',
+      rating: '9.83',
+      instructorPic: 'assets/woman3.jpeg'
+  ),
+  Instructor(
+      instructorName: 'Floyd Lyons',
+      rating: '9.82',
+      instructorPic: 'assets/man3.jpeg'
+  ),
+  Instructor(
+      instructorName: 'Charlie Casey',
+      rating: '9.77',
+      instructorPic: 'assets/woman4.jpeg'
+  ),
+  Instructor(
+      instructorName: 'Dustin Simon',
+      rating: '9.74',
+      instructorPic: 'assets/man4.jpeg'
+  ),
+  Instructor(
+      instructorName: 'Adele Lee',
+      rating: '9.71',
+      instructorPic: 'assets/man5.jpeg'
+  )
+];
