22c69f8a8babdd20ef3a5cdd802589f968650f8c
Niwarthana Nihathamani <niwarthana.2016285@iit.ac.lk>
2021-06-17 22:15:25 +0530

    2nd page done

--- lib/name_list.dart
+++ lib/name_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:surfing_instructor/detail.dart';
 import 'package:surfing_instructor/models/instructor.dart';
 
 class ListPage extends StatefulWidget {
@@ -15,50 +16,49 @@ class _ListPageState extends State<ListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFAF6ED),
-      body: ListView(
-        children: <Widget>[
-      Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: null,
+        backgroundColor: Color(0xFFFAF6ED),
+        body: ListView(children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: null,
+              ),
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: null,
+              )
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: null,
-          )
-        ],
-      ),
-      Padding(
-        padding: EdgeInsets.only(left: 15.0, right: 15.0),
-        child: Row(
-          children: <Widget>[
-            Text('Santa Monica, CA',
-                style: GoogleFonts.tinos(
-                    fontSize: 25.0, fontWeight: FontWeight.w500)),
-            IconButton(
-              icon: Icon(Icons.keyboard_arrow_down),
-              onPressed: null,
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15.0),
+            child: Row(
+              children: <Widget>[
+                Text('Santa Monica, CA',
+                    style: GoogleFonts.tinos(
+                        fontSize: 25.0, fontWeight: FontWeight.w500)),
+                IconButton(
+                  icon: Icon(Icons.keyboard_arrow_down),
+                  onPressed: null,
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      Padding(
-        padding: EdgeInsets.only(left: 15.0, right: 15.0),
-        child: Text('Best Surfing Instructors in Santa Monica, California!',
-            style: GoogleFonts.sourceSansPro(
-                fontSize: 15.0,
-                fontWeight: FontWeight.w400,
-                color: Color(0xFF5E5B54))),
-      ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15.0),
+            child: Text('Best Surfing Instructors in Santa Monica, California!',
+                style: GoogleFonts.sourceSansPro(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF5E5B54))),
+          ),
           Padding(
               padding: EdgeInsets.only(left: 15.0, right: 15.0),
               child: TextField(
                 decoration: InputDecoration(
                   prefixIcon:
-                  IconButton(icon: Icon(Icons.search), onPressed: null),
+                      IconButton(icon: Icon(Icons.search), onPressed: null),
                   hintText: 'Search',
                   hintStyle: GoogleFonts.sourceSansPro(
                       fontSize: 17.0,
@@ -82,70 +82,68 @@ class _ListPageState extends State<ListPage> {
                   }).toList()
                 ],
               ))
-
-    ]
-    ));
+        ]));
   }
 
-buildInstructorGrid(Instructor instructor) {
-  return GestureDetector(
-      onTap: () {
-        // Navigator.of(context).push(MaterialPageRoute(
-        //     builder: (context) => InstructorDetails(selectedInstructor: instructor)
-        // ));
-      },
-      child: Padding(
-          padding: EdgeInsets.all(5.0),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                  height: 150.0, width: 100.0, color: Colors.transparent),
-              Positioned(
-                  left: 30.0,
-                  top: 65.0,
-                  child: Container(
-                      height: 30.0,
-                      width: 40.0,
-                      decoration: BoxDecoration(boxShadow: [
-                        BoxShadow(
-                            blurRadius: 7.0,
-                            color: Colors.grey.withOpacity(0.75),
-                            offset: Offset(5, 25),
-                            spreadRadius: 12.0)
-                      ]))),
-              Positioned(
-                  left: 12.0,
-                  top: 15.0,
-                  child: Hero(
-                      tag: instructor.instructorPic,
-                      child: Container(
-                          height: 110.0,
-                          width: 85.0,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(7.0),
-                              image: DecorationImage(
-                                  image: AssetImage(instructor.instructorPic),
-                                  fit: BoxFit.cover))))),
-              Positioned(
-                  left: 22.0,
-                  top: 138.0,
-                  child: Column(
-                    children: <Widget>[
-                      Text(instructor.instructorName,
-                          style: GoogleFonts.sourceSansPro(fontSize: 12.0)),
-                      Row(children: [
-                        Icon(
-                          Icons.star,
-                          color: Colors.grey.withOpacity(0.5),
-                          size: 15.0,
-                        ),
-                        SizedBox(width: 3.0),
-                        Text(instructor.rating,
-                            style: GoogleFonts.sourceSansPro(fontSize: 11.0))
-                      ])
-                    ],
-                  ))
-            ],
-          )));
-}
+  buildInstructorGrid(Instructor instructor) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) =>
+                  InstructorDetailsPage(selectedInstructor: instructor)));
+        },
+        child: Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                    height: 150.0, width: 100.0, color: Colors.transparent),
+                Positioned(
+                    left: 30.0,
+                    top: 65.0,
+                    child: Container(
+                        height: 30.0,
+                        width: 40.0,
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              blurRadius: 7.0,
+                              color: Colors.grey.withOpacity(0.75),
+                              offset: Offset(5, 25),
+                              spreadRadius: 12.0)
+                        ]))),
+                Positioned(
+                    left: 12.0,
+                    top: 15.0,
+                    child: Hero(
+                        tag: instructor.instructorPic,
+                        child: Container(
+                            height: 110.0,
+                            width: 85.0,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7.0),
+                                image: DecorationImage(
+                                    image: AssetImage(instructor.instructorPic),
+                                    fit: BoxFit.cover))))),
+                Positioned(
+                    left: 22.0,
+                    top: 138.0,
+                    child: Column(
+                      children: <Widget>[
+                        Text(instructor.instructorName,
+                            style: GoogleFonts.sourceSansPro(fontSize: 12.0)),
+                        Row(children: [
+                          Icon(
+                            Icons.star,
+                            color: Colors.grey.withOpacity(0.5),
+                            size: 15.0,
+                          ),
+                          SizedBox(width: 3.0),
+                          Text(instructor.rating,
+                              style: GoogleFonts.sourceSansPro(fontSize: 11.0))
+                        ])
+                      ],
+                    ))
+              ],
+            )));
+  }
 }
