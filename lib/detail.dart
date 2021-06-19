22a470e4bdbaaf24ad28fbdfbfe63cd8a4f6a1f0
Niwarthana Nihathamani <niwarthana.2016285@iit.ac.lk>
2021-06-19 16:22:32 +0530

    bottom navigation done

--- lib/detail.dart
+++ lib/detail.dart
@@ -49,118 +49,161 @@ class _InstructorDetailsPageState extends State<InstructorDetailsPage> {
                     height: screenHeight / 3 + 25.0,
                     width: screenWidth,
                     child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          SizedBox(height: 25.0),
-                          Text(widget.selectedInstructor.instructorName,
-                              style: GoogleFonts.tinos(
-                                  fontSize: 25.0,
-                                  fontWeight: FontWeight.w500
-                              )
-                          ),
-                          SizedBox(height: 7.0),
-                          Text('Santa Monica, CA',
-                              style: GoogleFonts.sourceSansPro(
-                                  fontSize: 15.0,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xFF5E5B54)
-                              )
-                          ),
-                          SizedBox(height: 7.0),
-                          Row(
-                            children: <Widget>[
-                              // SmoothStarRating(
-                              //     allowHalfRating: false,
-                              //     starCount: 5,
-                              //     rating: 4.0,
-                              //     size: 15.0,
-                              //     color: Color(0xFFF36F32),
-                              //     borderColor: Color(0xFFF36F32),
-                              //     spacing:0.0
-                              // ),
-                              SizedBox(width: 3.0),
-                              Text('4.7',
-                                  style: GoogleFonts.sourceSansPro(
-                                      fontSize: 14.0,
-                                      fontWeight: FontWeight.w400,
-                                      color: Color(0xFFD57843)
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 25.0),
+                              Text(widget.selectedInstructor.instructorName,
+                                  style: GoogleFonts.tinos(
+                                      fontSize: 25.0,
+                                      fontWeight: FontWeight.w500
                                   )
                               ),
-                              SizedBox(width: 3.0),
-                              Text('(200 Reviews)',
+                              SizedBox(height: 7.0),
+                              Text('Santa Monica, CA',
+                                  style: GoogleFonts.sourceSansPro(
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.w400,
+                                      color: Color(0xFF5E5B54)
+                                  )
+                              ),
+                              SizedBox(height: 7.0),
+                              Row(
+                                children: <Widget>[
+                                  // SmoothStarRating(
+                                  //     allowHalfRating: false,
+                                  //     starCount: 5,
+                                  //     rating: 4.0,
+                                  //     size: 15.0,
+                                  //     color: Color(0xFFF36F32),
+                                  //     borderColor: Color(0xFFF36F32),
+                                  //     spacing:0.0
+                                  // ),
+                                  SizedBox(width: 3.0),
+                                  Text('4.7',
+                                      style: GoogleFonts.sourceSansPro(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w400,
+                                          color: Color(0xFFD57843)
+                                      )
+                                  ),
+                                  SizedBox(width: 3.0),
+                                  Text('(200 Reviews)',
+                                      style: GoogleFonts.sourceSansPro(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w400,
+                                          color: Color(0xFFC2C0B6)
+                                      )
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: 5.0),
+                              Text('Hello, My name is Cora ! I am from Santa Monica, California. I have more than 10 years of experience as a surf instructor and would love to help you...',
                                   style: GoogleFonts.sourceSansPro(
                                       fontSize: 14.0,
                                       fontWeight: FontWeight.w400,
-                                      color: Color(0xFFC2C0B6)
+                                      color: Color(0xFF201F1C)
                                   )
-                              )
+                              ),
+                              SizedBox(height: 5.0),
+                              Text('Read More',
+                                  style: GoogleFonts.sourceSansPro(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w400,
+                                      color: Color(0xFFF36F32)
+                                  )
+                              ),
                             ],
-                          ),
-                          SizedBox(height: 5.0),
-                          Text('Hello, My name is Cora ! I am from Santa Monica, California. I have more than 10 years of experience as a surf instructor and would love to help you...',
-                              style: GoogleFonts.sourceSansPro(
-                                  fontSize: 14.0,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xFF201F1C)
-                              )
-                          ),
-                          SizedBox(height: 5.0),
-                          Text('Read More',
-                              style: GoogleFonts.sourceSansPro(
-                                  fontSize: 14.0,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xFFF36F32)
-                              )
                           ),
                           SizedBox(height: 10.0),
                           Container(
-                              width: 150.0,
                               child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text(
-                                            '200',
-                                            style: GoogleFonts.tinos(
-                                                fontSize: 25.0,
-                                                fontWeight: FontWeight.w500
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Column(
+                                          children: <Widget>[
+                                            Text(
+                                                '200',
+                                                style: GoogleFonts.tinos(
+                                                    fontSize: 25.0,
+                                                    fontWeight: FontWeight.w500
+                                                )
+                                            ),
+                                            Text(
+                                                'Reviews',
+                                                style: GoogleFonts.sourceSansPro(
+                                                    fontSize: 15.0,
+                                                    fontWeight: FontWeight.w400,
+                                                    color: Color(0xFF5E5B54)
+                                                )
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(width: 24,),
+                                        Column(
+                                          children: <Widget>[
+                                            Text(
+                                                '4',
+                                                style: GoogleFonts.tinos(
+                                                    fontSize: 25.0,
+                                                    fontWeight: FontWeight.w500
+                                                )
+                                            ),
+                                            Text(
+                                                'Programs',
+                                                style: GoogleFonts.sourceSansPro(
+                                                    fontSize: 15.0,
+                                                    fontWeight: FontWeight.w400,
+                                                    color: Color(0xFF5E5B54)
+                                                )
+                                            )
+                                          ],
+                                        )
+                                      ]
+                                  ),
+
+                                  Align(
+                                      alignment: Alignment.bottomRight,
+                                      child: Container(
+                                        height: 58.0 +  MediaQuery.of(context).padding.bottom,
+                                        width: 100.0,
+                                        child: Center(
+                                            child: Column(
+                                                mainAxisAlignment: MainAxisAlignment.center,
+                                                children: [
+                                                  Text('>',
+                                                      style: GoogleFonts.sourceSansPro(
+                                                          fontSize: 20.0,
+                                                          fontWeight: FontWeight.w500,
+                                                          color: Color(0xFFFFF2D5)
+                                                      )
+                                                  ),
+                                                  Text('Availability',
+                                                      style: GoogleFonts.sourceSansPro(
+                                                          fontSize: 12.0,
+                                                          fontWeight: FontWeight.w500,
+                                                          color: Color(0xFFFFF2D5)
+                                                      )
+                                                  )
+                                                ]
                                             )
                                         ),
-                                        Text(
-                                            'Reviews',
-                                            style: GoogleFonts.sourceSansPro(
-                                                fontSize: 15.0,
-                                                fontWeight: FontWeight.w400,
-                                                color: Color(0xFF5E5B54)
-                                            )
-                                        )
-                                      ],
-                                    ),
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text(
-                                            '4',
-                                            style: GoogleFonts.tinos(
-                                                fontSize: 25.0,
-                                                fontWeight: FontWeight.w500
-                                            )
+                                        decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0)),
+                                            color: Color(0xFFFE6D2E)
                                         ),
-                                        Text(
-                                            'Programs',
-                                            style: GoogleFonts.sourceSansPro(
-                                                fontSize: 15.0,
-                                                fontWeight: FontWeight.w400,
-                                                color: Color(0xFF5E5B54)
-                                            )
-                                        )
-                                      ],
-                                    )
-                                  ]
+                                      )
+                                  ),
+                                ],
                               )
-                          )
+                          ),
+                          SizedBox(height: MediaQuery.of(context).padding.bottom,)
                         ]
                     ),
                     decoration: BoxDecoration(
@@ -169,38 +212,7 @@ class _InstructorDetailsPageState extends State<InstructorDetailsPage> {
                     ),
                   )
               ),
-              Align(
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                    height: 75.0,
-                    width: 100.0,
-                    child: Center(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('>',
-                                  style: GoogleFonts.sourceSansPro(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.w500,
-                                      color: Color(0xFFFFF2D5)
-                                  )
-                              ),
-                              Text('Availability',
-                                  style: GoogleFonts.sourceSansPro(
-                                      fontSize: 12.0,
-                                      fontWeight: FontWeight.w500,
-                                      color: Color(0xFFFFF2D5)
-                                  )
-                              )
-                            ]
-                        )
-                    ),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0)),
-                        color: Color(0xFFFE6D2E)
-                    ),
-                  )
-              ),
+
               Align(
                   alignment: Alignment.topLeft,
                   child: Padding(
